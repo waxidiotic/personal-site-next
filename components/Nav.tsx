1d25df0be38852b05a6db1a2508448cafca3f6ab
Alex Bussey <github@alexbussey.com>
2021-11-28 11:53:49 -0500

    options menu

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,3 +1,5 @@
+import { Options } from "./Options";
+
 export function Nav() {
   return (
     <nav className="flex items-center justify-between h-12">
@@ -30,30 +32,7 @@ export function Nav() {
           </a>
         </li>
         <li>
-          <div className="relative flex items-center">
-            <button
-              className="w-4 h-4 rounded bg-construction-accent"
-              id="color-menu-button"
-              aria-expanded="false"
-              aria-haspopup="true"
-            />
-          </div>
-        </li>
-        <li>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-6 h-6 text-construction-accent"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-            />
-          </svg>
+          <Options />
         </li>
       </ul>
     </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,21 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    colors: {
-      construction: {
-        dark: "#18181B",
-        accent: "#f59e0b",
-        text: "#e4e4e7",
+    extend: {
+      colors: {
+        construction: {
+          dark: "#18181B",
+          accent: "#f59e0b",
+          text: "#e4e4e7",
+        },
       },
+      stoke: (theme) => ({
+        construction: theme("colors.construction.accent"),
+      }),
+      boxShadow: (theme) => ({
+        avatar: `0px 0px 8px 8px ${theme("colors.construction.accent")}`,
+      }),
     },
-    stoke: (theme) => ({
-      construction: theme("colors.construction.accent"),
-    }),
-    boxShadow: (theme) => ({
-      avatar: `0px 0px 8px 8px ${theme("colors.construction.accent")}`,
-    }),
   },
   variants: {
     extend: {},
