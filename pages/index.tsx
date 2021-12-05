4ac0fcf9e247f010f27467df61a37f5935cd579d
Alex Bussey <github@alexbussey.com>
2021-12-05 18:03:57 -0500

    Actually make the favicon work

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,24 @@ const Home: NextPage = () => {
       <Head>
         <title>Alex Bussey</title>
         <meta name="description" content="Personal site for Alex Bussey" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main>
